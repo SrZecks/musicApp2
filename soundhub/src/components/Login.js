159f7d5599941ee4f9247016aa52dbc459e437c3
SrZecks <yuricordeiro@uni9.edu.br>
2020-03-13 17:24:14 -0300

    Heroku build

--- soundhub/src/components/Login.js
+++ soundhub/src/components/Login.js
@@ -19,6 +19,20 @@ export default class Login extends Component {
 
     componentDidMount() {
         M.AutoInit(); // Init materialize
+        gapi.auth2.authorize({
+            client_id: '558477949158-a9g4lrb4s4jhan64vfcdhji8nhfr0u89.apps.googleusercontent.com',
+            scope: 'email profile openid',
+            response_type: 'id_token permission'
+        }, function (response) {
+            if (response.error) {
+                // An error happened!
+                return;
+            }
+            // The user authorized the application for the scopes requested.
+            var accessToken = response.access_token;
+            var idToken = response.id_token;
+            // You can also now use gapi.client to perform authenticated requests.
+        });
     }
 
     handleChange = (e) => {
